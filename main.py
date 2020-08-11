a46bdfbf6f476eb702cd2f95cfde8712813b6412
Chuyang Zhou <czho4938@ac02xx0uvj1g9.internal.usyd.edu.au>
2020-08-11 18:59:00 +1000

    Finish main.py

--- main.py
+++ main.py
@@ -244,6 +244,7 @@ def train(train_loader, epoch, model1, optimizer1, model2, optimizer2):
         pure_ratio_list1.append(pure_ratio_1*100)
         pure_ratio_list2.append(pure_ratio_2*100)
 
+        # Calculate the gradient of loss_1(2) by back propagation
         optimizer1.zero_grad()
         loss_1.backward()
         optimizer1.step()
@@ -251,10 +252,110 @@ def train(train_loader, epoch, model1, optimizer1, model2, optimizer2):
         loss_2.backward()
         optimizer2.step()
 
-def evaluate():
-    pass
+        if (i+1) % args.print_freq == 0:
+            print('Epoch [{:d}/{:d}], Iter [{:d}/{:d}], Training Accuracy1: {:.4f}, Training Accuracy2: {:.4f}, Loss1: {:.4f}, Loss2: {:.4f}, Pure Ratio1: {:.4f}, Pure Ratio2: {:.4f}'.format(epoch+1,
+                                                                                                                 args.n_epoch, i+1, len(train_dataset)//batch_size, 
+                                                                                                                 prec1, prec2,
+                                                                                                                 loss_1.data[0],
+                                                                                                                 loss_2.data[0],
+                                                                                                                 np.sum(pure_ratio_list1)/len(pure_ratio_list1),
+                                                                                                                 np.sum(pure_ratio_list2)/len(pure_ratio_list2)))
+        
+        train_acc1 = float(train_correct)/float(train_total)
+        train_acc2 = float(train_correct2)/float(train_total2)
+
+        return train_acc1, train_acc2, pure_ratio_list1, pure_ratio_list2
+
+
+def evaluate(test_loader, model1, model2):
+    print('Evaluating {}...'.format(model_str))
+    model1.eval()
+    correct1 = 0
+    total1 = 0
+    for image, labels, _ in test_loader:
+        images = Variable(images).cuda()
+        logits1 = model1(images)
+        outputs1 = F.softmax(logits1, dim=1)
+        _, pred1 = torch.max(outputs1.data, 1)
+        total1 += labels.size(0)
+        correct1 += (pred1.cpu() == labels).sum()
+    
+    model2.eval()
+    correct2 = 0
+    total2 = 0
+    for image, labels, _ in test_loader:
+        images = Variable(images).cuda()
+        logits2 = model2(images)
+        outputs2 = F.softmax(logits2, dim=1)
+        _, pred2 = torch.max(outputs2.data, 1)
+        total2 += labels.size(0)
+        correct2 += (pred2.cpu() == labels).sum()
+
+    acc1 = 100*float(correct1)/float(total1)
+    acc2 = 100*float(correct2)/float(total2)
+
+    return acc1, acc2
 
 def main():
+    # Data Loader (Input Pipeline)
+    print('loading dataset...')
+    # drop_last=True will drop the last incomplete batch
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size, 
+                                               num_workers=args.num_workers,
+                                               drop_last=True,
+                                               shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                                batch_size=batch_size,
+                                                num_workers=args.num_workers,
+                                                drop_last=True,
+                                                shuffle=False)
+
+    # Define models
+    print('building model...')
+    cnn1 = CNN(input_channel=input_channel, n_outputs=num_classes)
+    cnn1.cuda()
+    print(cnn1.parameters)
+    optimizer1 = torch.optim.Adam(cnn1.parameters(), lr=learning_rate)
+    
+    cnn2 = CNN(input_channel=input_channel, n_outputs=num_classes)
+    cnn2.cuda()
+    print(cnn2.parameters)
+    optimizer2 = torch.optim.Adam(cnn2.parameters(), lr=learning_rate)
+
+    mean_pure_ratio1=0
+    mean_pure_ratio2=0
+
+    with open(txtfile, "a") as myfile:
+        myfile.write('epoch: train_acc1 train_acc2 test_acc1 test_acc2 pure_ratio1 pure_ratio2\n')
+
+    epoch=0
+    train_acc1=0
+    train_acc2=0
+    # evaluate models with random weights
+    test_acc1, test_acc2=evaluate(test_loader, cnn1, cnn2)
+    print('Epoch [{:d}/{:d}] Test Accuracy on the {} test images: Model1 {:.4f} %% Model2 {:.4f} %% Pure Ratio1 {:.4f} %% Pure Ratio2 {:.4f} %%'.format(epoch+1, args.n_epoch, len(test_dataset), test_acc1, test_acc2, mean_pure_ratio1, mean_pure_ratio2))
+    # save results
+    with open(txtfile, "a") as myfile:
+        myfile.write(str(int(epoch)) + ': '  + str(train_acc1) +' '  + str(train_acc2) +' '  + str(test_acc1) + " " + str(test_acc2) + ' '  + str(mean_pure_ratio1) + ' '  + str(mean_pure_ratio2) + "\n")
+
+    # training
+    for epoch in range(1, args.n_epoch):
+        # train models
+        cnn1.train()
+        adjust_learning_rate(optimizer1, epoch)
+        cnn2.train()
+        adjust_learning_rate(optimizer2, epoch)
+        train_acc1, train_acc2, pure_ratio_1_list, pure_ratio_2_list=train(train_loader, epoch, cnn1, optimizer1, cnn2, optimizer2)
+        # evaluate models
+        test_acc1, test_acc2=evaluate(test_loader, cnn1, cnn2)
+        # save results
+        mean_pure_ratio1 = sum(pure_ratio_1_list)/len(pure_ratio_1_list)
+        mean_pure_ratio2 = sum(pure_ratio_2_list)/len(pure_ratio_2_list)
+        print('Epoch [{:d}/{:d}] Test Accuracy on the {} test images: Model1 {:.4f} %% Model2 {:.4f} %%, Pure Ratio 1 {:.4f} %%, Pure Ratio 2 {:.4f} %%'.format(epoch+1, args.n_epoch, len(test_dataset), test_acc1, test_acc2, mean_pure_ratio1, mean_pure_ratio2))
+        with open(txtfile, "a") as myfile:
+            myfile.write(str(int(epoch)) + ': '  + str(train_acc1) +' '  + str(train_acc2) +' '  + str(test_acc1) + " " + str(test_acc2) + ' ' + str(mean_pure_ratio1) + ' ' + str(mean_pure_ratio2) + "\n")
     pass
 
 if __name__ == '__main__':
